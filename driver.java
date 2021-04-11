762bd23281882048edaffaee78d043781aff6f8e
Javier19-cmd <63107636+Javier19-cmd@users.noreply.github.com>
2021-04-10 19:25:44 -0600

    Agregando cositas al principio

---
+++ driver.java
@@ -0,0 +1,23 @@
+/** 
+ * @author Javier Sebastián Valle Balsells Carnet 20159 
+ * 
+ * Clase driver: En esta clase se va a gestionar todo lo relacionado a gestionar el archivo diccionario.text y el otro archivo con un documento en cualquier idioma (inglés, español, fraancés)
+ * 
+ * 
+ * El diccionario contiene asociaciones de palabras en inglés, español y francés. 
+ * Estas palabras proceden a pasar a un árbol en donde se guardó el diccionario de forma in-order. 
+ * 
+ * Por otro lado, debe haber un segundo archivo que contenga un doc con oraciones en cualquier idioma y se debe decidir el idioma de origen y el idioma destino. 
+ * Se debe traducir cada palabra al idioma destino. Si en caso hay una palabra que no está en el diccionario, entonces se deja intacta la palabra encerrada en astericos.
+ * 
+ * */ 
+
+import java.util.Scanner;
+import java.io.*; 
+import java.util.FileReader; 
+import java.io.BufferedReader;
+
+public class driver{
+    public static void main(String[] args) {
+        //Método main. 
+     }
